72d8338d055560945cfb2a67fba70417d03f64f6
Lynn-0310 <nguyenthuylinh310vt@gmail.com>
2025-04-14 16:12:01 +0700

    update

--- app/src/main/java/vn/edu/tlu/cse/ntl/soundplay/ui/PlaySongActivity.java
+++ app/src/main/java/vn/edu/tlu/cse/ntl/soundplay/ui/PlaySongActivity.java
@@ -1,7 +1,9 @@
 package vn.edu.tlu.cse.ntl.soundplay.ui;
 
+import android.animation.ObjectAnimator;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +21,7 @@ public class PlaySongActivity extends AppCompatActivity {
     private TextView txtSongTitle, txtArtist;
     private ImageView imgCover;
     private MediaPlayer mediaPlayer;
+    private ObjectAnimator rotationAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +42,19 @@ public class PlaySongActivity extends AppCompatActivity {
         txtArtist.setText(artist);
         Glide.with(this).load(thumbnail).into(imgCover);
 
+        // Tạo hiệu ứng xoay ảnh
+        rotationAnimator = ObjectAnimator.ofFloat(imgCover, "rotation", 0f, 360f);
+        rotationAnimator.setDuration(10000); // Quay 1 vòng trong 10 giây
+        rotationAnimator.setRepeatCount(ObjectAnimator.INFINITE); // Lặp vô hạn
+        rotationAnimator.setInterpolator(new LinearInterpolator());
+
         // Phát nhạc
         mediaPlayer = new MediaPlayer();
         try {
             mediaPlayer.setDataSource(url);
-            mediaPlayer.prepare(); // hoặc prepareAsync nếu bạn muốn dùng loading
+            mediaPlayer.prepare(); // hoặc dùng prepareAsync nếu muốn loading mượt hơn
             mediaPlayer.start();
+            rotationAnimator.start(); // Bắt đầu xoay ảnh khi phát nhạc
         } catch (IOException e) {
             e.printStackTrace();
             Toast.makeText(this, "Không thể phát nhạc", Toast.LENGTH_SHORT).show();
@@ -54,9 +64,18 @@ public class PlaySongActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
+        // Giải phóng media player
+        if (mediaPlayer != null) {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+            }
             mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        // Dừng xoay ảnh
+        if (rotationAnimator != null) {
+            rotationAnimator.cancel();
         }
     }
 }
